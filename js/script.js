b74e4437384bfe9003d05d2174814abdd32513db
Eduardo Salinas <eduardoluis93@gmail.com>
2021-09-19 21:50:39 +0100

    Fixed bug in script.js
    I was able to fix the bug that caused the navbar text to get messed up in larger screens if the user decided to touch the hamburguer menu button. However, the bug is only fixed if the user decides to close the menu on a phone screen before switching into a larger screen. If the user decides to leave the hamburguer menu open before switching into a larger screen, the bug will still be present.

--- js/script.js
+++ js/script.js
@@ -19,11 +19,25 @@ function openNavbar() {
     }
 }
   
-/* Set the width of the side navigation to 0 */
+/* Set the width of the side navigation to 0.
+
+I had a bug in which, if I used a small screen and touched the hamburger menu icon, the text of the navbar would get
+messed up on larger screens, and I would be forced to refresh the page to fix the text. 
+
+This was being caused because the div that contains the "sidenavbar" and "mySidenavbar" classes would get a "styles" tag
+after pressing the hamburguer menu icon or the "x" icon to close it. 
+
+To solve this, I will remove the "styles" tag when closing the side navbar, which is achieved by turning the "width" property
+into "null" (source: https://stackoverflow.com/questions/18691655/remove-style-on-element).
+
+However, this bug will still be present if the user decides to keep the hamburger menu activated while on a small screen, and 
+then deciding to change into a larger screen. This is currently only possible to do in the "Responsive Design" view of
+Firefox Developer Edition, since most broswers on laptops/desktop computers won't let you to scale down the width of the 
+browser below 415 px, which is when this bug would trigger.*/
 function closeNavbar() {
     var x, i;
     x = document.querySelectorAll(".mySidenavbar");
     for (i = 0; i < x.length; i++) {
-      x[i].style.width = "0";
+      x[i].style.width = null;
     }
 }
